7e3a18f1dbad1162c49e5d833567278d1c312a9f
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-05-12 19:28:50 +0300

    style : formating with prettier

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function main() {
     let versionList = document.getElementById('version-list');
-    for ( let i = 0; i <=20; i++ ) {
-        if ( i % 5 === 0 ) {
-            versionList.innerHTML += `<li> Minor Patch : 0.${i}.${i}</li>`
+    for (let i = 0; i <= 20; i++) {
+        if (i % 5 === 0) {
+            versionList.innerHTML += `<li> Minor Patch : 0.${i}.${i}</li>`;
         } else {
-            versionList.innerHTML += `<li> Bug Fix : 0.0.${i}</li>`
+            versionList.innerHTML += `<li> Bug Fix : 0.0.${i}</li>`;
         }
     }
 }
