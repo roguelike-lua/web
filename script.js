3afb3996b58c6235a0a49033d29dbec3b3a04396
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-05-12 18:35:18 +0300

    feat : add JS script appending versioning to HTML
    add script that appends the list items to the unordered list

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function main() {
-    alert('working');
+    let versionList = document.getElementById('version-list');
+    for ( let i = 0; i <=20; i++ ) {
+        if ( i % 5 === 0 ) {
+            versionList.innerHTML += `<li> Minor Patch : 0.${i}.${i}</li>`
+        } else {
+            versionList.innerHTML += `<li> Bug Fix : 0.0.${i}</li>`
+        }
+    }
 }
 
 main();
