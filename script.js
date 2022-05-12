1d7d17d23f67296ec061ff7ee3787a04c04597ed
Evangelos Gravanis <gravanisevangelos@gmail.com>
2022-05-12 19:48:32 +0300

    style : minor tweaks

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function main() {
     let versionList = document.getElementById('version-list');
-    for (let i = 0; i <= 20; i++) {
+    for (let i = 0; i <= 200; i++) {
         if (i % 5 === 0) {
             versionList.innerHTML += `<li> Minor Patch : 0.${i}.${i}</li>`;
         } else {
