70a4c671b792a2f41866a6ff4d91e5573929f8a3
roguelike-lua <dkarnikis@gmail.com>
2024-03-10 11:19:00 +0000

    update game build

--- game/game.js
+++ game/game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"22edc45f-7593-4c55-a171-f7fd58b56802","remote_package_size":8501007,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":8501007,"audio":false}]});
+  loadPackage({"package_uuid":"3541bd9a-42df-4c12-9811-77fd0bded641","remote_package_size":8500837,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":8500837,"audio":false}]});
 
 })();
